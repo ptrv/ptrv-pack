bd38cc678bf7b8ebd596b4ae8d756c8102dbcd4d
ptrv <mail@petervasil.net>
2012-06-02 12:44:10 +0200

    Add untabify to sclang mode.

--- config/supercollider.el
+++ config/supercollider.el
@@ -8,12 +8,12 @@
 
 (eval-after-load "w3m"
   '(progn
-         (define-key w3m-mode-map [left] 'backward-char)
-         (define-key w3m-mode-map [right] 'forward-char)
-         (define-key w3m-mode-map [up] 'previous-line)
-         (define-key w3m-mode-map [down] 'next-line)
-         (setq w3m-auto-show 1)
-         ))
+                 (define-key w3m-mode-map [left] 'backward-char)
+                 (define-key w3m-mode-map [right] 'forward-char)
+                 (define-key w3m-mode-map [up] 'previous-line)
+                 (define-key w3m-mode-map [down] 'next-line)
+                 (setq w3m-auto-show 1)
+                 ))
 
 ;; (setq w3m-key-binding 'info)
 (custom-set-variables
@@ -64,3 +64,18 @@
 
 (add-to-list 'ac-modes 'sclang-mode)
 (add-to-list 'ac-user-dictionary-files "~/.local/share/SuperCollider/sclang_completion_dict")
+
+(defun sclang-mode-untabify ()
+        (save-excursion
+          (goto-char (point-min))
+          (while (re-search-forward "[ \t]+$" nil t)
+                (delete-region (match-beginning 0) (match-end 0)))
+          (goto-char (point-min))
+          (if (search-forward "\t" nil t)
+                  (untabify (1- (point)) (point-max))))
+        nil)
+
+  (add-hook 'sclang-mode-hook
+            '(lambda ()
+               (make-local-variable 'write-contents-hooks)
+               (add-hook 'write-contents-hooks 'sclang-mode-untabify)))
