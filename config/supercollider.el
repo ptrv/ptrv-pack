a77f4ea5ad4b14f0124ce31e24046058c09f8e9a
ptrv <mail@petervasil.net>
2012-06-27 02:15:30 +0200

    Readd sclang-untabify function.

--- config/supercollider.el
+++ config/supercollider.el
@@ -74,19 +74,19 @@
 (add-hook 'sclang-mode-hook 'yas/minor-mode)
 (yas/load-directory (concat (live-pack-lib-dir) "snippets"))
 
-;; (defun sclang-mode-untabify ()
-;;   (save-excursion
-;;     (goto-char (point-min))
-;;     (while (re-search-forward "[ \t]+$" nil t)
-;;       (delete-region (match-beginning 0) (match-end 0)))
-;;     (goto-char (point-min))
-;;     (if (search-forward "\t" nil t)
-;;         (untabify (1- (point)) (point-max))))
-;;   nil)
+(defun sclang-mode-untabify ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "[ \t]+$" nil t)
+      (delete-region (match-beginning 0) (match-end 0)))
+    (goto-char (point-min))
+    (if (search-forward "\t" nil t)
+        (untabify (1- (point)) (point-max))))
+  nil)
 
-;; (add-hook 'sclang-mode-hook
-;;           '(lambda ()
-;;              (make-local-variable 'write-contents-hooks)
-;;              (add-hook 'write-contents-hooks 'sclang-mode-untabify)))
+(add-hook 'sclang-mode-hook
+          '(lambda ()
+             (make-local-variable 'write-contents-hooks)
+             (add-hook 'write-contents-hooks 'sclang-mode-untabify)))
 
 (require 'ext-scel)
