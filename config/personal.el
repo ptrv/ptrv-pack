79e8f369833b6aa79ce64df8dfb680a939f5ec1c
ptrv <mail@petervasil.net>
2012-06-27 02:39:41 +0200

    Fix indentation?

--- config/personal.el
+++ config/personal.el
@@ -21,8 +21,8 @@
                ;; ("improcess apps"  (filename . "Development/improcess/apps"))
                ;; ("improcess lib"   (filename . "Development/improcess/lib"))
                ("dired" (mode . dired-mode))
-
                ))))
+
 (add-hook 'ibuffer-mode-hook
           (lambda ()
             (ibuffer-switch-to-saved-filter-groups "default")))
@@ -34,6 +34,13 @@
 (setq c-basic-offset 4)     ; indents 4 chars
 (setq tab-width 4)          ; and 4 char wide for TAB
 (setq indent-tabs-mode nil) ; And force use of spaces
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))))
 
 ; (add-hook 'write-file-hooks
 ;           (lambda () (if (not indent-tabs-mode)
