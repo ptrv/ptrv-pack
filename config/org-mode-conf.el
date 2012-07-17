7ca1435a9ee870d8ac4a00221552b44bdb8b5e58
ptrv <mail@petervasil.net>
2012-07-18 01:49:23 +0200

    Add org-mode bindings.

--- config/org-mode-conf.el
+++ config/org-mode-conf.el
@@ -3,6 +3,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (setq org-log-done t)
 (setq org-replace-disputed-keys t)
 ;; Override
