4b08b8971a82bfa98b248ae6a4418db3ae0b42b3
ptrv <mail@petervasil.net>
2012-08-02 13:44:49 +0200

    Set to fullscreen on small display.

--- config/x11-config.el
+++ config/x11-config.el
@@ -7,7 +7,12 @@
                  '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
                  '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
-;; (toggle-fullscreen)
+
+;; set fullscreen if we are on a small display
+(if (and
+     (<= (x-display-pixel-width) 1280)
+     (<= (x-display-pixel-height) 800))
+    (toggle-fullscreen))
 
 ;; Define a function for making desktop notifications
 (require 'dbus)
