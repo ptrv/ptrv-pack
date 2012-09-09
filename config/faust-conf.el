9028a1ff49dc1f353b45e68804f18e7d73649324
ptrv <mail@petervasil.net>
2012-09-09 12:53:50 +0200

    Add Synth-A-Modeler mode.

--- config/faust-conf.el
+++ config/faust-conf.el
@@ -2,3 +2,7 @@
 
 (setq auto-mode-alist (cons '("\\.dsp$" . faust-mode) auto-mode-alist))
 (autoload 'faust-mode "faust-mode" "FAUST editing mode." t)
+
+;; Synth-A-Modeler mode
+(setq auto-mode-alist (cons '("\\.mdl$" . sam-mode) auto-mode-alist))
+(autoload 'sam-mode "sam-mode" "Synth-A-Modeler editing mode." t)
