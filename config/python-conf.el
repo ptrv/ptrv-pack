f0015b2d8f60593c58784a81ff21c8039241862e
ptrv <mail@petervasil.net>
2012-11-01 14:10:57 +0100

    Fix python-conf.el.

--- config/python-conf.el
+++ config/python-conf.el
@@ -2,33 +2,52 @@
 (setq py-install-directory (concat (live-pack-lib-dir) "python-mode"))
 
 (require 'python-mode)
+;; ; use IPython
+;; (setq-default py-shell-name "ipython")
+;; (setq-default py-which-bufname "IPython")
+;; ; use the wx backend, for both mayavi and matplotlib
+;; (setq py-python-command-args
+;;   '("--gui=wx" "--pylab=wx" "--colors=Linux"))
+;; (setq py-force-py-shell-name-p t)
+
+;; ; switch to the interpreter after executing code
+;; (setq py-shell-switch-buffers-on-execute-p t)
+;; (setq py-switch-buffers-on-execute-p t)
+;; ; don't split windows
+;; (setq py-split-windows-on-execute-p nil)
+;; ; try to automagically figure out indentation
+;; (setq py-smart-indentation t)
+
+; pymacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
 
 (setq py-load-pymacs-p t)
 (setq py-start-run-py-shell nil)
 
+;; (setenv "PYMACS_PYTHON" "python2.7")
+; ropemacs
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
 ;; (require 'smart-operator)
 
-;; ;; (live-add-pack-lib "pymacs-0.24-beta2")
-;; ;; (require 'pymacs)
-;; (autoload 'pymacs-apply "pymacs")
-;; (autoload 'pymacs-call "pymacs")
-;; (autoload 'pymacs-eval "pymacs" nil t)
-;; (autoload 'pymacs-exec "pymacs" nil t)
-;; (autoload 'pymacs-load "pymacs" nil t)
-;; (pymacs-load "ropemacs" "rope-")
-;; (setq ropemacs-enable-autoimport t)
+;; (add-hook 'python-mode-hook
+;;           (lambda ()
+;;             (setq ac-sources
+;;                   '(ac-source-dictionary
+;;                     ac-source-words-in-buffer
+;;                     ac-source-words-in-same-mode-buffers
+;;                     ac-source-words-in-all-buffer
+;;                     ;; ac-source-yasnippet
+;;                     ac-source-semantic))))
 
-(add-hook 'python-mode-hook
-          (lambda ()
-            (setq ac-sources
-                  '(ac-source-dictionary
-                    ac-source-words-in-buffer
-                    ac-source-words-in-same-mode-buffers
-                    ac-source-words-in-all-buffer
-                    ;; ac-source-yasnippet
-                    ac-source-semantic))))
-
-(add-to-list 'ac-modes 'python-mode)
+;; (add-to-list 'ac-modes 'python-mode)
 
 ;; (add-hook 'python-mode-hook 'flymake-mode)
 ;; (when (load "flymake" t)
