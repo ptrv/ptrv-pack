fe5e2723f8eba52958914649657dc8610bfa931b
ptrv <mail@petervasil.net>
2012-11-15 13:37:22 +0100

    Add modified gandalf theme.

--- config/look-and-feel.el
+++ config/look-and-feel.el
@@ -4,7 +4,6 @@
 (global-linum-mode t)
 (setq linum-format (if on-console "%4d " "%4d"))
 
-
 ;; Show column numbers in modeline
 (setq column-number-mode t)
 
@@ -16,3 +15,7 @@
 (defun linum-on ()
   (unless (or (minibufferp) (member major-mode linum-disabled-modes))
     (linum-mode 1)))
+
+(setq ptrv-themes-dir (concat (live-pack-lib-dir) "themes"))
+(load-file (concat  (file-name-as-directory ptrv-themes-dir) "gandalf-ptrv.el"))
+(color-theme-gandalf-ptrv)
