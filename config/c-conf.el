9533bcd1508f3b8820ad3f2f88728cf867d2513d
ptrv <mail@petervasil.net>
2012-11-21 00:26:33 +0100

    Load clang-complete only for c++-mode.

--- config/c-conf.el
+++ config/c-conf.el
@@ -1,38 +1,45 @@
 (live-add-pack-lib "clang-complete-async")
 
 ;; Hook auto-complete into clang
-;; (eval-after-load "cc-mode"
-;;   '(progn
-;;      (require 'auto-complete-clang-async)
-;;      (setq clang-complete-executable
-;;            (concat (live-pack-lib-dir) "clang-complete-async/clang-complete"))
-;;      (when (not (file-exists-p clang-complete-executable))
-;;        (warn (concat "The clang-complete executable doesn't exist - please run "
-;;                      dotfiles-dir "setup.sh to compile it.")))
-;;      ;; Add Qt4 includes to load path if installed
-;;      (when (file-exists-p "/usr/include/qt4")
-;;        (setq ac-clang-flags
-;;              (mapcar (lambda (f) (concat "-I" f))
-;;                      (directory-files "/usr/include/qt4" t "Qt\\w+"))))))
+(eval-after-load "cc-mode"
+  '(progn
+     (require 'auto-complete-clang-async)
+     (setq clang-complete-executable
+           ;; "~/.live-packs/ptrv-pack/lib/clang-complete-async/clang-complete"
+           (concat (live-pack-lib-dir) "clang-complete-async/clang-complete")
+           )
+     (when (not (file-exists-p clang-complete-executable))
+       (warn (concat "The clang-complete executable doesn't exist")))
+     ;; (when (not (file-exists-p clang-complete-executable))
+     ;;   (warn (concat "The clang-complete executable doesn't exist - please run "
+     ;;                 dotfiles-dir "setup.sh to compile it.")))
+     ;; Add Qt4 includes to load path if installed
+     (when (file-exists-p "/usr/include/qt4")
+       (setq ac-clang-flags
+             (mapcar (lambda (f) (concat "-I" f))
+                     (directory-files "/usr/include/qt4" t "Qt\\w+"))))
+     ))
 
-;; (add-hook 'c++-mode-hook
-;;           (lambda ()
-;;             (setq 'ac-sources 'ac-source-clang-async)
-;;             (launch-completion-proc)))
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (add-to-list 'ac-sources 'ac-source-clang-async)
+            (launch-completion-proc)))
 
-(require 'auto-complete-clang-async)
+;; (require 'auto-complete-clang-async)
 
-(defun ac-cc-mode-setup ()
-  (setq clang-complete-executable "~/.live-packs/ptrv-pack/lib/clang-complete-async/clang-complete")
-  (when (not (file-exists-p clang-complete-executable))
-    (warn (concat "The clang-complete executable doesn't exist")))
-  (setq ac-sources '(ac-source-clang-async))
-  (launch-completion-proc)
-)
+;; (defun ac-cc-mode-setup ()
+;;   (setq clang-complete-executable "~/.live-packs/ptrv-pack/lib/clang-complete-async/clang-complete")
+;;   (when (not (file-exists-p clang-complete-executable))
+;;     (warn (concat "The clang-complete executable doesn't exist")))
+;;   (setq ac-sources '(ac-source-clang-async))
+;;   (launch-completion-proc)
+;; )
 
-(defun my-ac-config ()
-  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-  (global-auto-complete-mode t))
+;; (defun my-ac-config ()
+;;   ;;(add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+;;   (add-hook 'c-mode-hook 'ac-cc-mode-setup)
+;;   (add-hook 'c++-mode-hook 'ac-cc-mode-setup)
+;;   (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+;;   (global-auto-complete-mode t))
 
-(my-ac-config)
+;; (my-ac-config)
