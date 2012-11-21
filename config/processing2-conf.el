cebf5774b0f636b477c3a706a12cf275dba00340
ptrv <mail@petervasil.net>
2012-11-21 18:53:30 +0100

    Remove tab/indent with code in processing2-conf.el.

--- config/processing2-conf.el
+++ config/processing2-conf.el
@@ -4,15 +4,6 @@
 (add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
 (setq processing-location "~/applications/processing-2.0/processing-java")
 
-;; (add-hook 'processing-mode-hook '(lambda ()
-;;                                    (setq c-basic-offset 2)
-;;                                    (set (make-local-variable 'tab-width) 2)
-;;                                    (set (make-local-variable 'tab-stop-list)
-;;                                         '(2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84))))
-(add-hook 'processing-mode-hook '(lambda ()
-                                   (setq c-basic-offset 2)
-                                   (set (make-local-variable 'tab-width) 2)))
-
 (yas-load-directory (concat (live-pack-lib-dir) "processing2-emacs/snippets"))
 
 (add-hook 'processing-mode-hook
