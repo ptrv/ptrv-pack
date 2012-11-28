9d09c223b2a2789f2474e0e38e9dded9a3b99e98
ptrv <mail@petervasil.net>
2012-11-28 11:44:10 +0100

    Some tweaks for ibuffer.

--- config/personal.el
+++ config/personal.el
@@ -26,53 +26,57 @@
 
 ;;organise ibuffer into handy groups
 (setq ibuffer-saved-filter-groups
-      (quote (("default"
-               ("IRC"      (mode . erc-mode))
-               ("emacs" (or
-                         (name . "^\\*scratch\\*$")
-                         (name . "^\\*Messages\\*$")
-                         (name . "^\\*Completions\\*$")
-                         (filename . ".emacs.d")
-                         (filename . ".live-packs")))
-
-               ;; ("improcess apps"  (filename . "Development/improcess/apps"))
-               ;; ("improcess lib"   (filename . "Development/improcess/lib"))
-               ("magit" (name . "\\*magit"))
-               ("dired" (mode . dired-mode))
-               ;; ("sclang" (mode . sclang-mode))
-               ))))
+	  (quote (("default"
+			   ("IRC"      (mode . erc-mode))
+			   ("emacs" (or
+						 (name . "^\\*scratch\\*$")
+						 (name . "^\\*Messages\\*$")
+						 (name . "^\\*Completions\\*$")
+						 (filename . ".emacs.d")
+						 (filename . ".live-packs")))
+			   ("magit" (name . "\\*magit"))
+			   ("dired" (mode . dired-mode))
+			   ("sclang" (mode . sclang-mode))
+			   ("Org" (mode . org-mode))
+			   ("Help" (or (name . "\\*Help\\*")
+						   (name . "\*Apropos\\*")
+						   (name . "\\*info\\*")))
+			   ))))
 
 (add-hook 'ibuffer-mode-hook
-          (lambda ()
-            (ibuffer-switch-to-saved-filter-groups "default")))
+		  (lambda ()
+			(ibuffer-auto-mode 1)
+			(ibuffer-switch-to-saved-filter-groups "default")))
+
+(setq ibuffer-show-empty-filter-groups nil)
 
 ;; popwin settings
 (setq popwin:special-display-config
-      '(("*Help*" :height 30 :stick t)
-        ("*Completions*" :noselect t)
-        ("*compilation*" :noselect t)
-        ("*Messages*")
-        ("*Occur*" :noselect t)
-        ("\\*Slime Description.*" :noselect t :regexp t :height 30)
-        ("*magit-commit*" :noselect t :height 30 :width 80)
-        ("*magit-diff*" :noselect t :height 30 :width 80)
-        ("*magit-edit-log*" :noselect t :height 15 :width 80)
-        ("\\*Slime Inspector.*" :regexp t :height 30)
-        ("*Ido Completions*" :noselect t :height 30)
-        ("*eshell*" :height 30)
-        ("\\*ansi-term\\*.*" :regexp t :height 30)
-        ("*shell*" :height 30)
-        (".*overtone.log" :regexp t :height 30)
-        ("*gists*" :height 30)
-        ("*sldb.*":regexp t :height 30)
-        ("*Gofmt Errors*" :noselect t)
-        ("\\*godoc*" :regexp t)
-        ("*Shell Command Output*" :noselect t)
-        ("*nREPL error*" :height 30)
-        ("*nREPL doc*" :height 30)
-        ("*Kill Ring*" :height 30)
-        ("*project-status*" :noselect t)
-        ("*Compile-Log" :height 20)))
+	  '(("*Help*" :height 30 :stick t)
+		("*Completions*" :noselect t)
+		("*compilation*" :noselect t)
+		("*Messages*")
+		("*Occur*" :noselect t)
+		("\\*Slime Description.*" :noselect t :regexp t :height 30)
+		("*magit-commit*" :noselect t :height 30 :width 80)
+		("*magit-diff*" :noselect t :height 30 :width 80)
+		("*magit-edit-log*" :noselect t :height 15 :width 80)
+		("\\*Slime Inspector.*" :regexp t :height 30)
+		("*Ido Completions*" :noselect t :height 30)
+		("*eshell*" :height 30)
+		("\\*ansi-term\\*.*" :regexp t :height 30)
+		("*shell*" :height 30)
+		(".*overtone.log" :regexp t :height 30)
+		("*gists*" :height 30)
+		("*sldb.*":regexp t :height 30)
+		("*Gofmt Errors*" :noselect t)
+		("\\*godoc*" :regexp t)
+		("*Shell Command Output*" :noselect t)
+		("*nREPL error*" :height 30)
+		("*nREPL doc*" :height 30)
+		("*Kill Ring*" :height 30)
+		("*project-status*" :noselect t)
+		("*Compile-Log" :height 20)))
 
 (setq live-disable-zone t)
 
@@ -88,19 +92,19 @@
 (defun toggle-unkillable-scratch-buffer-erase ()
   (interactive)
   (if unkillable-scratch-buffer-erase
-      (progn
-        (setq unkillable-scratch-buffer-erase nil)
-        (message "Disable scratch-buffer erase on kill!"))
-    (progn
-      (setq unkillable-scratch-buffer-erase t)
-      (message "Enable scratch-buffer erase on kill!"))))
+	  (progn
+		(setq unkillable-scratch-buffer-erase nil)
+		(message "Disable scratch-buffer erase on kill!"))
+	(progn
+	  (setq unkillable-scratch-buffer-erase t)
+	  (message "Enable scratch-buffer erase on kill!"))))
 
 (defun unkillable-scratch-buffer ()
   (if (equal (buffer-name (current-buffer)) "*scratch*")
-      (progn
-        (if unkillable-scratch-buffer-erase
-            (delete-region (point-min) (point-max)))
-        nil
-        )
-    t))
+	  (progn
+		(if unkillable-scratch-buffer-erase
+			(delete-region (point-min) (point-max)))
+		nil
+		)
+	t))
 (add-hook 'kill-buffer-query-functions 'unkillable-scratch-buffer)
