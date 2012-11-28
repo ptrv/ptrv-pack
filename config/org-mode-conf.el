291baa7316e80c823ab7f704bb8f5ebb705d3b6f
ptrv <mail@petervasil.net>
2012-11-28 17:59:59 +0100

    Minor org-mode clock and bindings changes.

--- config/org-mode-conf.el
+++ config/org-mode-conf.el
@@ -11,14 +11,18 @@
 
 (setq org-log-done t)
 (setq org-replace-disputed-keys nil)
+(setq org-clock-into-drawer t)
+(setq org-clock-idle-time 10)
+(setq org-src-fontify-natively nil)
 ;; Override
 (add-hook 'org-mode-hook
           (lambda()
             (local-set-key [(control meta return)] 'org-insert-heading)
             (local-set-key [(control shift left)] 'previous-buffer)
             (local-set-key [(control shift right)] 'next-buffer)
-            (local-set-key [(meta shift right)] 'ido-switch-buffer)
-            (local-set-key [(meta shift left)] 'magit-status))
+            ;; (local-set-key [(meta shift right)] 'ido-switch-buffer)
+            ;; (local-set-key [(meta shift left)] 'magit-status)
+            )
           )
 
 (add-hook 'org-mode-hook 'turn-off-flyspell)
