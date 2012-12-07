e062c38f3ec3c948cac0b42e8a66dfd8e080966d
ptrv <mail@petervasil.net>
2012-12-07 23:53:49 +0100

    Fix markdown-mode file extension recognition.

--- config/markdown-conf.el
+++ config/markdown-conf.el
@@ -3,8 +3,14 @@
 (autoload 'markdown-mode "markdown-mode.el"
    "Major mode for editing Markdown files" t)
 
-(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
+;; (setq-default markdown-command
+;;               (concat
+;;                "pandoc -S -s --self-contained -f markdown -t html5 --css="
+;;                markdown-css-path
+;;                " "))
+
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 
 (setq markdown-css-path (expand-file-name
                          (concat
