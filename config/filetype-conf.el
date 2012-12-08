6dce3d5e876f3a6796300de4ad940dc8cb1ec463
ptrv <mail@petervasil.net>
2012-12-09 00:04:02 +0100

    Add snippet-mode expressions to auto-mode-alist

--- config/filetype-conf.el
+++ config/filetype-conf.el
@@ -13,9 +13,15 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
+;; Apache config
 (autoload 'apache-mode "apache-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
 (add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
 (add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
 (add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
 (add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
+
+;; Snippets
+(add-to-list 'auto-mode-alist '("lib/snippets" . snippet-mode))
+(add-to-list 'auto-mode-alist '("etc/snippets" . snippet-mode))
+(add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
