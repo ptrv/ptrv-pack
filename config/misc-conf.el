a60b796315b012d92cbb7ba3a9acaf69bc2c6f8f
ptrv <mail@petervasil.net>
2012-12-09 00:01:45 +0100

    Add more webjump sites

--- config/misc-conf.el
+++ config/misc-conf.el
@@ -79,6 +79,31 @@
                    "www.stackoverflow.com"
                    "http://stackoverflow.com/search?q="
                    ""])
+                 ("askubuntu" .
+                  [simple-query
+                   "www.askubuntu.com"
+                   "http://askubuntu.com/search?q="
+                   ""])
+                 ("superuser" .
+                  [simple-query
+                   "www.superuser.com"
+                   "http://superuser.com/search?q="
+                   ""])
+                 ("tex.stackexchange" .
+                  [simple-query
+                   "tex.stackexchange.com"
+                   "http://tex.stackexchange.com/search?q="
+                   ""])
+                 ("math.stackexchange" .
+                  [simple-query
+                   "math.stackexchange.com"
+                   "http://math.stackexchange.com/search?q="
+                   ""])
+                 ("leo" .
+                  [simple-query
+                   "dict.leo.org"
+                   "http://dict.leo.org/ende?search="
+                   ""])
                  ("Java API" .
                   [simple-query
                    "www.google.com"
