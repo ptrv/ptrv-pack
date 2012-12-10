456783c444e8386a03ab19ddcd5ce34d2a8343d6
ptrv <mail@petervasil.net>
2012-12-10 19:50:08 +0100

    Edit git commit messages with git-commit-mode

--- config/filetype-conf.el
+++ config/filetype-conf.el
@@ -1,7 +1,7 @@
 (add-to-list 'auto-mode-alist '("\\.zsh-template$" . shell-script-mode))
 (add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
 
-(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
+;;(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
