28be5a580a868b379e2e68380f784ce4be525699
ptrv <mail@petervasil.net>
2012-12-31 00:59:07 +0100

    Make file executable if has shebang

--- config/buffer-conf.el
+++ config/buffer-conf.el
@@ -41,3 +41,17 @@
         )
     t))
 (add-hook 'kill-buffer-query-functions 'unkillable-scratch-buffer)
+
+;; make file executabable on save if has shebang
+(add-hook 'after-save-hook
+          #'(lambda ()
+              (and (save-excursion
+                     (save-restriction
+                       (widen)
+                       (goto-char (point-min))
+                       (save-match-data
+                         (looking-at "^#!"))))
+                   (not (file-executable-p buffer-file-name))
+                   (shell-command (concat "chmod u+x " (shell-quote-argument buffer-file-name)))
+                   (message
+                    (concat "Saved as script: " buffer-file-name)))))
