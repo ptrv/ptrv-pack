68329f3d734b941f6fd1a696e36e7315a1464f52
ptrv <mail@petervasil.net>
2012-12-31 00:53:27 +0100

    Add lua-mode.

--- config/flycheck-conf.el
+++ config/flycheck-conf.el
@@ -101,5 +101,5 @@
 (add-hook 'nxhtml-mode-hook 'flycheck-mode)
 (add-hook 'LaTeX-mode-hook 'flycheck-mode)
 (add-hook 'python-mode-hook 'flycheck-mode)
+(add-hook 'lua-mode-hook 'flycheck-mode)
 ;; (add-hook 'go-mode-hook 'flycheck-mode)
-;; (add-hook 'lua-mode-hook 'flycheck-enable-except-on-temp-buffers)
--- config/misc-conf.el
+++ config/misc-conf.el
@@ -117,3 +117,8 @@
 
 (setq browse-url-generic-program (executable-find "google-chrome")
       browse-url-browser-function 'browse-url-generic)
+
+;; lua-mode
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
