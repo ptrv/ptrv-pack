3d854c7feecc56c8400dd249d925bb286aba9b50
ptrv <mail@petervasil.net>
2013-01-04 12:05:26 +0100

    Don't switch on flycheck on html and xml temp buffers.

--- config/flycheck-conf.el
+++ config/flycheck-conf.el
@@ -17,10 +17,17 @@
 ;; ;; modes
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-hook 'nxml-mode-hook 'flycheck-mode)
-(add-hook 'sgml-mode-hook 'flycheck-mode)
-(add-hook 'nxhtml-mode-hook 'flycheck-mode)
+(defun flycheck-enable-except-on-temp-buffers ()
+  (if (and buffer-file-name (file-exists-p buffer-file-name))
+      (flycheck-mode)))
+
+(add-hook 'nxml-mode-hook 'flycheck-enable-except-on-temp-buffers)
+(add-hook 'sgml-mode-hook 'flycheck-enable-except-on-temp-buffers)
+(add-hook 'nxhtml-mode-hook 'flycheck-enable-except-on-temp-buffers)
+;; (add-hook 'sgml-mode-hook 'flycheck-mode)
+;; (add-hook 'nxhtml-mode-hook 'flycheck-mode)
 (add-hook 'LaTeX-mode-hook 'flycheck-mode)
 (add-hook 'python-mode-hook 'flycheck-mode)
 (add-hook 'lua-mode-hook 'flycheck-mode)
 ;; (add-hook 'go-mode-hook 'flycheck-mode)
+(add-hook 'shell-script-mode 'flycheck-mode)
