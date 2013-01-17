7099af5cb776fb299962d21350b78f867fd59cb1
ptrv <mail@petervasil.net>
2013-01-17 12:52:44 +0100

    Use require instead of load in pandoc-conf.el

--- config/pandoc-conf.el
+++ config/pandoc-conf.el
@@ -1,5 +1,5 @@
 (live-add-pack-lib "pandoc-mode")
-(load "pandoc-mode")
+(require 'pandoc-mode)
 (add-hook 'pandoc-mode-hook 'pandoc-load-default-settings)
 
 (add-hook 'markdown-mode-hook 'turn-on-pandoc)
