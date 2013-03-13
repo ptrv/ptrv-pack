054f0bcda839a3fefe620926f43398dce972d00e
ptrv <mail@petervasil.net>
2013-03-13 11:35:09 +0100

    Add goflymake flycheck checker.

--- config/golang-conf.el
+++ config/golang-conf.el
@@ -16,3 +16,18 @@
             ))
 
 ;;(define-key go-mode-map (kbd "C-c h") 'godoc)
+
+(eval-after-load "go-mode"
+  '(if (executable-find "goflymake")
+       (progn
+         (flycheck-declare-checker go-goflymake
+           "A Go syntax and style checker using the go utility.
+
+See URL `https://github.com/dougm/goflymake'."
+           :command '("goflymake" source)
+           :error-patterns '(("^\\(?1:.*\\):\\(?2:[0-9]+\\): \\(?4:.*\\)$" error))
+           :modes 'go-mode)
+         ;;(add-to-list 'flycheck-checkers 'go-goflymake)
+         (setq flycheck-checkers (append flycheck-checkers '(go-goflymake)))
+         )
+     (warn "goflymake not found")))
