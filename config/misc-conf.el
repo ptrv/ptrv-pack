14501f020ab85930ebc83001ce85c3d57fb067c9
ptrv <mail@petervasil.net>
2013-03-17 12:02:16 +0100

    Clean and tidy-up misc-conf

--- config/misc-conf.el
+++ config/misc-conf.el
@@ -1,34 +1,26 @@
 ;; misc-conf.el
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; session.el
 ;; save a list of open files in $HOME/.emacs.desktop
 ;; save the desktop file automatically if it already exists
 (setq desktop-save (quote if-exists))
 (desktop-save-mode 1)
 
-;; ;; Flymake
-;; (add-hook 'find-file-hook 'flymake-find-file-hook)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; misc
+
+;; default browser
+(setq browse-url-generic-program (executable-find "google-chrome")
+      browse-url-browser-function 'browse-url-generic)
 
 ;; ido recent file
 (setq ido-max-directory-size 100000)
 
 (setq sentence-end-double-space nil)
 
-;; gist.el
-(setq gist-view-gist t)
-(add-to-list 'gist-supported-modes-alist '(processing-mode . "pde"))
-(add-to-list 'gist-supported-modes-alist '(conf-mode . "desktop"))
-
 ;; follow version controlled symlinks automatically
 (setq vc-follow-symlinks t)
-
-;; auto-complete
-(setq ac-dwim t)
-(setq ac-ignore-case nil)
-;;(setq ac-auto-start 3)
-(setq ac-auto-show-menu t)
-(setq ac-auto-start nil)
-(ac-set-trigger-key "TAB")
 
 (setq live-disable-zone t)
 
@@ -39,6 +31,26 @@
 ;;(setq compilation-scroll-output 'first-error)
 (setq compilation-scroll-output t)
 
+;; disabled commands
+(put 'downcase-region 'disabled nil)
+(put 'updacase-region 'disabled nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; gist.el
+(setq gist-view-gist t)
+(add-to-list 'gist-supported-modes-alist '(processing-mode . "pde"))
+(add-to-list 'gist-supported-modes-alist '(conf-mode . "desktop"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; auto-complete
+(setq ac-dwim t)
+(setq ac-ignore-case nil)
+;;(setq ac-auto-start 3)
+(setq ac-auto-show-menu t)
+(setq ac-auto-start nil)
+(ac-set-trigger-key "TAB")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; fix tramp backups
 (add-to-list 'bkup-backup-directory-info
              (list tramp-file-name-regexp ""))
@@ -47,6 +59,7 @@
 ;; ;;disable backups of files edited with tramp
 ;;(setq tramp-bkup-backup-directory-info nil)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; hippie-expand functions list
 ;; http://trey-jackson.blogspot.de/2007/12/emacs-tip-5-hippie-expand.html
 (setq hippie-expand-try-functions-list
@@ -62,12 +75,15 @@
         try-complete-lisp-symbol
         ))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; magit
 ;; newline after 72 chars in magit-log-edit-mode
 (add-hook 'magit-log-edit-mode-hook
           (lambda ()
              (set-fill-column 72)
              (auto-fill-mode 1)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;; Soft word wrap
 ;; (require 'longlines)
 ;; (setq longlines-show-hard-newlines t)
@@ -79,22 +95,19 @@
 (live-add-pack-lib "arduino-mode")
 (require 'arduino-mode)
 
-(setq browse-url-generic-program (executable-find "google-chrome")
-      browse-url-browser-function 'browse-url-generic)
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; refheap
 (require 'my-secrets)
 (require 'refheap)
 
-;; disabled commands
-(put 'downcase-region 'disabled nil)
-(put 'updacase-region 'disabled nil)
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; insert-time
 (live-add-pack-lib "insert-time")
 (require 'insert-time)
 (setq insert-date-format "%Y-%m-%d")
 (setq insert-time-format "%H:%M:%S")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; pomodoro.el
 (require 'pomodoro)
 (pomodoro-add-to-mode-line)
@@ -102,6 +115,7 @@
 (setq pomodoro-break-start-sound (concat ptrv-pack-root-dir "etc/sounds/alarm.wav"))
 (setq pomodoro-work-start-sound (concat ptrv-pack-root-dir "etc/sounds/alarm.wav"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ChucK
 (live-add-pack-lib "chuck-mode")
 (require 'chuck-mode)
@@ -116,9 +130,13 @@
         (whitespace-tab-width tab-width))
     ad-do-it))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; the silver searcher
 (require 'ag)
 (setq ag-highlight-search t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; switch between sqlite3 and spatialite excutable
 (defun sql-switch-spatialite-sqlite ()
   (interactive)
   (let* ((sqlprog sql-sqlite-program)
@@ -128,8 +146,11 @@
     (setq sql-sqlite-program change)
     (message "sql-sqlite-program changed to %s" change)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (live-add-pack-lib "misc")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; pure-mode
 (when (require 'pure-mode nil 'noerror)
   (require 'pure-mode)
@@ -144,25 +165,34 @@
   (define-key pure-mode-map (kbd "C-c M-p") 'run-pure)
   (define-key pure-mode-map (kbd "C-x M-p") 'pure-scratchpad))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; mercurial
 (require 'ahg)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; make nxml outline work with gpx files
-(defun gpx-setup ()
-  (when (and (stringp buffer-file-name)
-             (string-match "\\.gpx\\'" buffer-file-name))
-    (make-local-variable 'nxml-section-element-name-regexp)
-    (setq nxml-section-element-name-regexp "trk\\|trkpt\\|wpt")
-    (make-local-variable 'nxml-heading-element-name-regexp)
-    (setq nxml-heading-element-name-regexp "name\\|time")
-    ))
-(add-hook 'nxml-mode-hook 'gpx-setup)
+(eval-after-load 'nxml-mode
+  '(progn
+     (defun gpx-setup ()
+       (when (and (stringp buffer-file-name)
+                  (string-match "\\.gpx\\'" buffer-file-name))
+         (make-local-variable 'nxml-section-element-name-regexp)
+         (setq nxml-section-element-name-regexp "trk\\|trkpt\\|wpt")
+         (make-local-variable 'nxml-heading-element-name-regexp)
+         (setq nxml-heading-element-name-regexp "name\\|time")
+         ))
+     (add-hook 'nxml-mode-hook 'gpx-setup)
+     (setq nxml-slash-auto-complete-flag t)))
 
-(setq nxml-slash-auto-complete-flag t)
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; lambda-mode
 (require 'lambda-mode)
 (setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
 
 (add-hook 'emacs-lisp-mode-hook #'lambda-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; autopair.el
 (autopair-global-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
