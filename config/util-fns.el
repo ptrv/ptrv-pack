69b444866fccf2817ee7e45e0fbcade9babaa93a
ptrv <mail@petervasil.net>
2013-03-21 12:27:52 +0100

    Add magit-toggle-whitespace function

--- config/util-fns.el
+++ config/util-fns.el
@@ -341,7 +341,25 @@ If mark is activate, duplicate region lines below."
      ;;   (kill-buffer)
      ;;   (jump-to-register :magit-fullscreen))
      ;; (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
-    ))
+
+     (defun magit-toggle-whitespace ()
+       (interactive)
+       (if (member "-w" magit-diff-options)
+           (magit-dont-ignore-whitespace)
+         (magit-ignore-whitespace)))
+
+     (defun magit-ignore-whitespace ()
+       (interactive)
+       (add-to-list 'magit-diff-options "-w")
+       (magit-refresh))
+
+     (defun magit-dont-ignore-whitespace ()
+       (interactive)
+       (setq magit-diff-options (remove "-w" magit-diff-options))
+       (magit-refresh))
+
+     (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+     ))
 
 (defun goto-line-with-feedback ()
   "Show line numbers temporarily, while prompting for the line number input"
