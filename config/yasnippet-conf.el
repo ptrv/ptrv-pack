5f874408d9538283f2a4557d848fbffeca95c51d
ptrv <mail@petervasil.net>
2013-03-25 14:00:25 +0100

    Modify yas-keymap

--- config/yasnippet-conf.el
+++ config/yasnippet-conf.el
@@ -10,3 +10,14 @@
   (interactive)
   (yas-load-directory live-yasnippet-dir)
   (yas-load-directory ptrv-yasnippet-dir))
+
+(eval-after-load 'yasnippet
+  '(progn
+     (setq yas-keymap  (let ((map (make-sparse-keymap)))
+                         (define-key map [(control tab)] 'yas-next-field-or-maybe-expand)
+                         (define-key map (kbd "C-TAB")   'yas-next-field-or-maybe-expand)
+                         (define-key map [(shift tab)]   'yas-prev-field)
+                         (define-key map [backtab]       'yas-prev-field)
+                         (define-key map (kbd "C-g")     'yas-abort-snippet)
+                         (define-key map (kbd "C-d")     'yas-skip-and-clear-or-delete-char)
+                         map))))
