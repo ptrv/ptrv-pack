7b8f9ddc69139e026fb742f94543f6c89b3eccac
ptrv <mail@petervasil.net>
2013-03-26 19:25:18 +0100

    Clean-up golang-conf.el

--- config/golang-conf.el
+++ config/golang-conf.el
@@ -1,32 +1,27 @@
-;; go-lang completion
-(require 'go-autocomplete)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; golang-conf.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-hook 'go-mode-hook '(lambda ()
-                           (set (make-local-variable 'before-save-hook)
-                                'gofmt-before-save)))
-
-;;(define-key go-mode-map (kbd "C-c h") 'godoc)
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (exec-path-from-shell-copy-env "GOROOT")
 (exec-path-from-shell-copy-env "GOPATH")
 
-(eval-after-load "go-mode"
-  '(if (executable-find "goflymake")
-       (progn
-         (flycheck-declare-checker go-goflymake
-           "A Go syntax and style checker using the go utility.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; go-lang completion
+(add-to-list 'load-path (concat (getenv "GOPATH")
+                                "/src/github.com/nsf/gocode/emacs"))
+(require 'go-autocomplete)
 
-See URL `https://github.com/dougm/goflymake'."
-           :command '("goflymake" "flycheck-" source-inplace)
-           :error-patterns '(("^\\(?1:.*\\):\\(?2:[0-9]+\\): \\(?4:.*\\)$" error))
-           :modes 'go-mode)
-         (add-to-list 'flycheck-checkers 'go-goflymake)
-         )
-     (warn "goflymake not found")))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; hooks
+(add-hook 'go-mode-hook (lambda ()
+                          (make-local-variable 'before-save-hook)
+                          (setq before-save-hook 'gofmt-before-save)))
 (add-to-list 'go-mode-hook 'hs-minor-mode)
 (add-to-list 'go-mode-hook 'flycheck-mode)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; compile fucntions
 (defun go-build ()
   "compile project"
   (interactive)
@@ -47,7 +42,24 @@ See URL `https://github.com/dougm/goflymake'."
   (interactive)
     (compile (concat "go run " buffer-file-name)))
 
-(define-key go-mode-map (kbd "C-c C-r") 'go-run)
-(define-key go-mode-map (kbd "C-c C-b") 'go-build)
-(define-key go-mode-map (kbd "C-c C-t") 'go-test)
-(define-key go-mode-map (kbd "C-c C-u") 'go-chk)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(eval-after-load "go-mode"
+  '(progn
+     (define-key go-mode-map (kbd "C-c C-r") 'go-run)
+     (define-key go-mode-map (kbd "C-c C-b") 'go-build)
+     (define-key go-mode-map (kbd "C-c C-t") 'go-test)
+     (define-key go-mode-map (kbd "C-c C-u") 'go-chk)
+
+     ;; flycheck goflymake checker
+     (flycheck-declare-checker go-goflymake
+             "A Go syntax and style checker using the go utility.
+
+See URL `https://github.com/ptrv/goflymake'."
+             :command '("goflymake" "flycheck-" source-inplace)
+             :error-patterns '(("^\\(?1:.*\\):\\(?2:[0-9]+\\): \\(?4:.*\\)$" error))
+             :modes 'go-mode)
+     (add-to-list 'flycheck-checkers 'go-goflymake)
+     ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
