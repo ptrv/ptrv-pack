20e80151f457033fa88b154a475856f32e0deb98
ptrv <mail@petervasil.net>
2013-04-10 01:25:44 +0200

    Update package-conf

--- config/package-conf.el
+++ config/package-conf.el
@@ -12,61 +12,72 @@
 
 (package-initialize)
 
+(defadvice package-compute-transaction (before package-compute-transaction-reverse
+                                               (package-list requirements)
+                                               activate compile)
+  "reverse the requirements"
+  (setq requirements (reverse requirements))
+  (print requirements))
+
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(magit
-                      ;;auctex
-                      wrap-region
-                      go-mode
-                      starter-kit-eshell
-                      ack-and-a-half
-                      session
-                      notify
-                      edit-server
-                      color-theme
-                      ;;org-plus-contrib
-                      markdown-mode
-                      js2-mode
-                      apache-mode
-                      yaml-mode
-                      xml-rpc
-                      pytest
-                      git-commit-mode
-                      gitignore-mode
-                      gitconfig-mode
-                      gnuplot
-                      dtrt-indent
-                      exec-path-from-shell
-                      s
-                      dash
-                      ido-ubiquitous
-                      refheap
-                      epc
-                      iflipb
-                      find-file-in-project
-                      pomodoro
-                      flycheck
-                      pandoc-mode
-                      iedit
-                      glsl-mode
-                      ag
-                      cmake-mode
-                      pylint
-                      lua-mode
-                      ahg
-                      autopair
-                      tea-time
-                      json-mode
-                      iy-go-to-char
-                      projectile
-                      popwin
-                      edit-server-htmlize
-                      move-text
-                      pcmpl-git
-                      )
+(defvar ptrv-packages '(magit
+                        ;;auctex
+                        wrap-region
+                        go-mode
+                        starter-kit-eshell
+                        ack-and-a-half
+                        session
+                        notify
+                        edit-server
+                        color-theme
+                        ;;org-plus-contrib
+                        markdown-mode
+                        js2-mode
+                        apache-mode
+                        yaml-mode
+                        xml-rpc
+                        pytest
+                        git-commit-mode
+                        gitignore-mode
+                        gitconfig-mode
+                        gnuplot
+                        dtrt-indent
+                        exec-path-from-shell
+                        s
+                        dash
+                        ido-ubiquitous
+                        refheap
+                        epc
+                        iflipb
+                        find-file-in-project
+                        pomodoro
+                        flycheck
+                        pandoc-mode
+                        iedit
+                        glsl-mode
+                        ag
+                        cmake-mode
+                        pylint
+                        lua-mode
+                        ahg
+                        autopair
+                        tea-time
+                        json-mode
+                        iy-go-to-char
+                        projectile
+                        popwin
+                        edit-server-htmlize
+                        move-text
+                        pcmpl-git
+                        multiple-cursors
+                        expand-region
+                        w3m
+                        flymake-cursor
+                        )
   "A list of packages to ensure are installed at launch.")
 
-(dolist (p my-packages)
+(dolist (p ptrv-packages)
   (when (not (package-installed-p p))
     (package-install p)))
