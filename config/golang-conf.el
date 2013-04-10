7ce46fdf49597b26a73c4a71897cd54da29bc0a6
ptrv <mail@petervasil.net>
2013-04-11 01:36:43 +0200

    Add elisp version of goflymake checker

--- config/golang-conf.el
+++ config/golang-conf.el
@@ -75,3 +75,36 @@
 ;;            (replace-match (file-name-nondirectory filename) t t nil 1))
 ;;          (compilation-mode)
 ;;          (display-buffer errbuf)))))
+
+(eval-after-load "flycheck"
+  '(progn
+   (defun go-flycheck-check-command ()
+     "Build go checker command depending on the type of source file to check."
+     (let ((src-tmp-file (file-name-nondirectory
+                          (flycheck-substitute-argument 'source-inplace)))
+           (src-orig (file-name-nondirectory (buffer-file-name)))
+           (go-list-format (concat "{{range .GoFiles}}{{.}},{{end}}"
+                                   "{{range .CgoFiles}}{{.}},{{end}}"))
+           args files-list go-list-result)
+       (if (s-ends-with? "_test.go" src-tmp-file)
+           (progn
+             (setq args (list "test" "-c"))
+             (setq go-list-format (concat go-list-format
+                                          "{{range .TestGoFiles}}{{.}},{{end}}")))
+         (setq args (list "build" "-o" "/dev/null")))
+       (setq args (append args `(,src-tmp-file)))
+       (setq go-list-result (process-lines "go" "list" "-f" go-list-format))
+       (setq files-list (s-split "," (car go-list-result) t))
+       (setq files-list (remove src-orig files-list))
+       (setq files-list (remove src-tmp-file files-list))
+       (setq args (append args files-list))))
+
+   (flycheck-declare-checker go-goflycheck
+     "A Go syntax and style checker using the go compiler.
+
+See URL `https://golang.org/cmd/go'."
+     :command (append (list "go") `((eval (go-flycheck-check-command))))
+     :error-patterns '(("^\\(?1:.*\\):\\(?2:[0-9]+\\): \\(?4:.*\\)$" error))
+     :modes 'go-mode)
+
+   (add-to-list 'flycheck-checkers 'go-goflycheck)))
