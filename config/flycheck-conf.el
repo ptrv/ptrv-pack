991f90cf600b4bfec1c45d7c00415bd358788655
ptrv <mail@petervasil.net>
2013-04-11 12:08:14 +0200

    Add c++ flycheck checker using cpplint.py and some clean-ups

--- config/flycheck-conf.el
+++ config/flycheck-conf.el
@@ -20,10 +20,20 @@
 (add-hook 'sgml-mode-hook 'flycheck-enable-except-on-temp-buffers)
 ;;(add-hook 'nxhtml-mode-hook 'flycheck-enable-except-on-temp-buffers)
 (add-hook 'sgml-mode-hook 'flycheck-mode)
-;; (add-hook 'nxhtml-mode-hook 'flycheck-mode)
 (add-hook 'LaTeX-mode-hook 'flycheck-mode)
 (add-hook 'python-mode-hook 'flycheck-mode)
 (add-hook 'lua-mode-hook 'flycheck-mode)
-;; (add-hook 'go-mode-hook 'flycheck-mode)
 (add-hook 'sh-mode-hook 'flycheck-mode)
-;;(add-hook 'go-mode-hook 'flycheck-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; cpplint.py checker
+(flycheck-declare-checker c++-cpplint
+  "A cpp syntax checker using the cpplin.py tool.
+
+See URL `https://code.google.com/p/google-styleguide/'."
+  :command '("cpplint.py" "--verbose=3" source-original)
+  :error-patterns '(("^\\(?1:.*\\):\\(?2:.*\\):  \\(?4:.*\\)$" warning))
+  :modes 'c++-mode)
+
+(add-to-list 'flycheck-checkers 'c++-cpplint)
+(add-hook 'c++-mode-hook 'flycheck-mode)
