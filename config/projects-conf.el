f5e87eee8c9d13bee91e5127650e483477de24e1
ptrv <mail@petervasil.net>
2013-04-12 01:14:44 +0200

    Add projectile as submodule
    dont use head revision because C-c p f is buggy

--- config/projects-conf.el
+++ config/projects-conf.el
@@ -4,5 +4,6 @@
 ;; (make-directory (file-name-as-directory (concat live-tmp-dir "mk-project-cache")) t)
 ;; (load "~/.emacs-projects.el" 'noerror)
 
+(live-add-pack-lib "projectile")
 (require 'projectile)
 (projectile-global-mode)
