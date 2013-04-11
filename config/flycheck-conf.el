10b91e75a526fb2c4f2c2f5f6d1f2eb9c1abed60
ptrv <mail@petervasil.net>
2013-04-12 01:15:04 +0200

    Minor clean-ups in flycheck-conf

--- config/flycheck-conf.el
+++ config/flycheck-conf.el
@@ -14,12 +14,10 @@
 
 (defun flycheck-enable-except-on-temp-buffers ()
   (if (and buffer-file-name (file-exists-p buffer-file-name))
-      (flycheck-mode)))
+      (flycheck-mode 1)))
 
 (add-hook 'nxml-mode-hook 'flycheck-enable-except-on-temp-buffers)
 (add-hook 'sgml-mode-hook 'flycheck-enable-except-on-temp-buffers)
-;;(add-hook 'nxhtml-mode-hook 'flycheck-enable-except-on-temp-buffers)
-(add-hook 'sgml-mode-hook 'flycheck-mode)
 (add-hook 'LaTeX-mode-hook 'flycheck-mode)
 (add-hook 'python-mode-hook 'flycheck-mode)
 (add-hook 'lua-mode-hook 'flycheck-mode)
